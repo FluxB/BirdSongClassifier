84a18b079adeefb4d0da398378820b54db596663
FluxB <FluxB@users.noreply.github.com>
2016-08-21 18:11:58 +0200

    deleted script from preprocessor, unit tests adjusted

--- preprocessor.py
+++ preprocessor.py
@@ -44,48 +44,9 @@ class Preprocessor(object):
         preprocessed_spectrograms = []
         for spec in spectrograms:
             preprocessed_spec = spec
-            preprocessed_spec = self.bg_subtraction(preprocessed_spec)
             # preprocessed_spec = np.log(spec**2)
             preprocessed_spec /= np.max(preprocessed_spec)
             preprocessed_spectrograms.extend([preprocessed_spec])
 
         return preprocessed_spectrograms
 
-    def bg_subtraction(self,s):
-
-        mask,vector = self.create_mask(s)
-
-        #signal
-        s_signal = s
-        s_signal[np.logical_not(mask)] = 0
-        s_signal = s_signal[...,vector]
-        return s_signal
-        #background
-    
-        s_bg = s[...,np.logical_not(vector)]
-        
-    def create_mask(self,s): # Creates mask to kill background noise and quite times
-        nb_f, nb_t = len(s[:,0]), len(s[0,:])
-        
-        # normalize naively
-        sn=np.true_divide(s, np.max(s))
-        
-        
-        # to Set zero values smaller than 3 times fixed-t-median or smaller 3 times fixed-f-median
-        m_f = np.tile(np.median(sn,axis=0),(nb_f,1))
-        m_t = np.transpose(np.tile(np.transpose(np.median(sn,axis=1)),(nb_t,1)))
-
-        
-       
-        mask_inv = np.logical_or((sn < 3 * m_f),(sn < 3 * m_t))
-        mask = np.logical_not(mask_inv)
-        
-        #morphological trafos: dilation and erosion
-        mask = morph.binary_erosion(mask,structure=np.ones((4,4)),iterations=1)
-        mask = morph.binary_dilation(mask,structure=np.ones((4,4)),iterations=1)
-
-        #make continous twittering by use of killig vector :)
-        vector = np.sum(mask,axis=0)>0
-        vector = morph.binary_dilation(vector,structure=np.ones((1)),iterations=1)
-        
-        return mask, vector
--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from spectrogram import plot_spect
+from data_preparation import DataPreparator
 from preprocessor import Preprocessor
 
 
@@ -9,15 +10,17 @@ class Test_bg_subtraction(unittest.TestCase):
     def test_bg_subtraction(self):
         p=Preprocessor(10)
         s=p.load_npy('./test.npy')
-        mask, vector = p.create_mask(s)
+        generator = DataPreparator("", "", 512)
+        mask, vector = generator.create_mask(s)
         self.assertGreater(np.sum(vector), 120)
 
     def test_bg_subtraction2(self):
         p = Preprocessor(10)
         s = p.load_npy('./test.npy')
+        generator = DataPreparator("", "", 512)
         samples1=len(s[0,:])
         
-        snew = p.bg_subtraction(s)
+        snew = generator.bg_subtraction(s)
         samples2=len(snew[0,:])
 
         self.assertGreater(samples1, samples2)
