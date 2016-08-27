97feeb30cb651891368ae41213f9df76b56d357e
Florian Niedermann <florian.niedermann@gmail.com>
2016-08-27 19:48:51 +0100

    naiv keras model added

--- train.py
+++ train.py
@@ -6,15 +6,30 @@ import matplotlib.pyplot as plt
 import multiprocessing as mp
 import time
 import random
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution1D, MaxPooling1D
+from keras.utils import np_utils
+
+
 
 # main class of programm, starts and organizes the training
 class Bird(object):
 
     # label_path: path to label file which holds the path of all samples and their class id
-    def __init__(self, label_path):
-        self.label_path = sys.argv[1]
+    def __init__(self, label_path, meta_path, output_path):
+        self.label_path = label_path
+        self.meta_path = meta_path
+        self.output_path = output_path
+        
         self.batch_size = 32
         self.queue_size = 2048
+
+        self.nb_filters = 32
+        self.nb_kernel_size = 5
+        self.t_pool = 2
+        self.f_pool = 3
+
         self.nr_epoch = 1
         self.preprocessor = Preprocessor(10)
         self.augmenter = AugmentTransform(20, 10)
@@ -40,6 +55,8 @@ class Bird(object):
             labels.append(label)
             self.inverse_labels.setdefault(label, []).append(path)
 
+        self.nb_species = len(inverse_labels)
+            
         for line in f_bg:
             line = line.strip()
             (path, label) = line.split(" ")
@@ -51,10 +68,15 @@ class Bird(object):
         
         return (paths, labels)
 
-
+    def load_meta_data(self, meta_path):
+        meta = open(meta_path, "r")
+        (path, sr, self.nb_f_steps, self.nb_t_steps) = f[0].split(",")
+        
     # loads and randomizes data
     def load_data(self):
         (paths, labels) = self.load_labels(self.label_path)
+        self.load_meta_data(self.meta_path)
+        
         self.nr_files = len(paths)
 
         mask = np.arange(self.nr_files)
@@ -93,10 +115,30 @@ class Bird(object):
 
     
     # start training process
-    def train(self):
+    def train(self, output_path):
         self.load_data()
         self.start_queue_filling_process()
 
+        model = Sequential()
+        model.add(Convolution1D(self.nb_filters,self.nb_kernel_size,border_mode='valid',input_shape=(self.nb_f_steps,self.nb_t_steps)))
+        model.add(Activation('relu'))
+        model.add(Convolution1D(self.nb_filters,self.nb_kernel_size))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(self.f_pool,self.t_pool)))
+        model.add(Dropout(0.25))
+        
+        model.add(Flatten())
+        model.add(Dense(128)) #How many neurons needed?
+        model.add(Activation('relu'))
+
+        model.add(Dropout(0.5))
+        model.add(Dense(self.nb_species))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
+
+        
+        
         nr_batches = self.nr_files // self.batch_size
         
         for epoch in range(self.nr_epoch):
@@ -109,11 +151,16 @@ class Bird(object):
                     label_batch.append(label)
                     plt.imshow(spec)
                     plt.show()
-                #START TRAINING HERE
+                model.fit(spec_batch,label_batch, verbose=1)
+
+        model.save(self.output_path)
+
 
 
 if __name__ == "__main__":
     label_path = sys.argv[1]
-    bird = Bird(label_path)
+    meta_path = sys.argv[2]
+    output_path = sys.argv[3]
+    bird = Bird(label_path,meta_path,output_path)
     bird.train()
             
