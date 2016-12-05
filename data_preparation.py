d6ebd4cd85d073ebff8b0c0ccd08ced7ce27c3b7
FluxB <FluxB@users.noreply.github.com>
2016-12-05 18:40:03 +0100

    added preprocessing of clef data

--- data_preparation.py
+++ data_preparation.py
@@ -13,6 +13,8 @@ from spectrogram import spectrogram
 import scipy.ndimage.morphology as morph
 import matplotlib.pyplot as plt
 
+import xml.etree.ElementTree as ET
+
 
 class DataPreparator(object):
 
@@ -21,15 +23,14 @@ class DataPreparator(object):
         self.out_path = out_path
         self.chunk_duration = chunk_duration
 
-
-    def prepareTrainingData(self):   
+    def prepareTrainingData(self):
         f_label = open("labels.txt", "w")
         f_label_bg = open("labels_bg.txt", "w")
         f_meta = open("meta.txt", "w")
 
         if not os.path.isdir(self.out_path):
             os.mkdir(self.out_path)
-        if not os.path.isdir(self.out_path + "/bg"):    
+        if not os.path.isdir(self.out_path + "/bg"):
             os.mkdir(self.out_path + "/bg")
 
         folder_names = os.listdir(self.data_path)
@@ -38,7 +39,7 @@ class DataPreparator(object):
         for folder in folder_names:
             if not os.path.isdir(self.data_path + folder) or folder[0] == ".":
                 continue
-    
+
             print("Preparing folder: ", folder)
             wavs = os.listdir(self.data_path + folder)
 
@@ -47,75 +48,98 @@ class DataPreparator(object):
             for wav in wavs:
                 if not os.path.isfile(self.data_path + folder + "/" + wav) or wav[0] == ".":
                     continue
-                print("Preparing file: ", wav)
-                y, sr = librosa.load(self.data_path + folder + "/" + wav)
-                spec = spectrogram(y)
+                self.__process_wav(wav, label_dict, folder, f_label, f_label_bg, f_meta)
 
-                # normalize naively
-                spec=np.true_divide(spec, np.max(spec))
-                
-                spec,bg = self.bg_subtraction(spec)
-
-                chunks = self.make_chunks(spec)
-                
-                name, ftype = wav.split(".")
-                for i, chunk in enumerate(chunks):
-                    out_fname = self.out_path + "/" + name + "_" + str(i) + ".npy"
-                    np.savetxt(out_fname, chunk)
-                    f_label.write(out_fname + " " + str(label_dict[folder]) + "\n")
-                    f_meta.write(out_fname + " " + str(sr) + " " + str(chunk.shape[0]) + " " + str(chunk.shape[1]) + "\n")
-                
-                if bg.shape[1] == 0 or spec.shape[1] == 0:
-                    continue
-                bg=np.true_divide(bg, np.max(spec))
-                chunks_bg = self.make_chunks(bg)
-
-                for i, chunk in enumerate(chunks_bg):
-                    out_fname = self.out_path + name + "_bg_" + str(i) + ".npy"
-                    f_label_bg.write(out_fname + " " + str(label_dict[folder]) + "\n")
-                    np.savetxt(out_fname, chunk)
-                    
         pickle.dump(label_dict, open("label_dict.pickle", "wb"))
 
+    def prepareCLEFTrainingData(self):
+        f_label = open("labels.txt", "w")
+        f_label_bg = open("labels_bg.txt", "w")
+        f_meta = open("meta.txt", "w")
 
-    def bg_subtraction(self,s):
+        if not os.path.isdir(self.out_path):
+            os.mkdir(self.out_path)
+        if not os.path.isdir(self.out_path + "/bg"):
+            os.mkdir(self.out_path + "/bg")
 
-        mask,vector = self.create_mask(s)
+        label_dict = {}
+        file_names = os.listdir(self.data_path)
 
-        #s_bg = np.array(s,copy=True)
-        
-        #signal projection
-        #s[np.logical_not(mask)] = 0
-        s_signal = s[...,vector]
+        for file_name in file_names:
+            if file_name[0] == ".":
+                continue
+            tree = ET.parse(self.data_path + "/" + file_name)
+            root = tree.getroot()
+            class_name = root.find("ClassId").text
+            wav = root.find("FileName").text
+            metadata = ""
+            for child in root:
+                metadata += child.text + ","
+            self.__process_wav(wav, label_dict, class_name, f_label, f_label_bg, f_meta, metadata)
 
-        #background projection
-        s_bg = s[...,np.logical_not(vector)]
+    def __process_wav(self, wav, label_dict, class_name, f_label, f_label_bg, f_meta, additional_meta=""):
+        print("Preparing file: ", wav)
+        y, sr = librosa.load(self.data_path + class_name + "/" + wav)
+        spec = spectrogram(y)
 
-        return s_signal,s_bg
-        
-    
-    def create_mask(self,s): # Creates mask to kill background noise and quiet times
-        nb_f, nb_t = len(s[:,0]), len(s[0,:])
+        # normalize naively
+        specb= np.true_divide(spec, np.max(spec))
+
+        spec, bg = self.bg_subtraction(spec)
+
+        chunks = self.make_chunks(spec)
+
+        name, ftype = wav.split(".")
+        for i, chunk in enumerate(chunks):
+            out_fname = self.out_path + "/" + name + "_" + str(i) + ".npy"
+            np.savetxt(out_fname, chunk)
+            f_label.write(out_fname + " " + str(label_dict[class_name]) + "\n")
+            f_meta.write("{} {} {} {} {}\n".format(out_fname, sr, chunk.shape[0], chunk.shape[1], additional_meta))
+
+        if bg.shape[1] == 0 or spec.shape[1] == 0:
+            continue
+        bg = np.true_divide(bg, np.max(spec))
+        chunks_bg = self.make_chunks(bg)
+
+        for i, chunk in enumerate(chunks_bg):
+            out_fname = self.out_path + name + "_bg_" + str(i) + ".npy"
+            f_label_bg.write(out_fname + " " + str(label_dict[class_name]) + "\n")
+            np.savetxt(out_fname, chunk)
+
+    def bg_subtraction(self, s):
+
+        mask, vector = self.create_mask(s)
+
+        # s_bg = np.array(s,copy=True)
+
+        # signal projection
+        # s[np.logical_not(mask)] = 0
+        s_signal = s[..., vector]
+
+        # background projection
+        s_bg = s[..., np.logical_not(vector)]
+
+        return s_signal, s_bg
+
+    def create_mask(self, s):  # Creates mask to kill background noise and quiet times
+        nb_f, nb_t = len(s[:, 0]), len(s[0, :])
 
         # Set to zero values smaller than 3 times fixed-t-median or smaller 3 times fixed-f-median
-        m_f = np.tile(np.median(s,axis=0),(nb_f,1))
-        m_t = np.transpose(np.tile(np.transpose(np.median(s,axis=1)),(nb_t,1)))
+        m_f = np.tile(np.median(s, axis=0), (nb_f, 1))
+        m_t = np.transpose(np.tile(np.transpose(np.median(s, axis=1)), (nb_t, 1)))
 
-
-
-        mask_inv = np.logical_or((s < 3 * m_f),(s < 3 * m_t))
+        mask_inv = np.logical_or((s < 3 * m_f), (s < 3 * m_t))
         mask = np.logical_not(mask_inv)
 
-        #morphological trafos: dilation and erosion
-        mask = morph.binary_erosion(mask,structure=np.ones((4,4)),iterations=1)
-        mask = morph.binary_dilation(mask,structure=np.ones((4,4)),iterations=1)
+        # morphological trafos: dilation and erosion
+        mask = morph.binary_erosion(mask, structure=np.ones((4, 4)), iterations=1)
+        mask = morph.binary_dilation(mask, structure=np.ones((4, 4)), iterations=1)
 
-        #make continous twittering by use of killig vector :)
-        vector = np.sum(mask,axis=0)>0
-        vector = morph.binary_dilation(vector,structure=np.ones((2)),iterations=1)
+        # make continous twittering by use of killig vector :)
+        vector = np.sum(mask, axis=0) > 0
+        vector = morph.binary_dilation(vector, structure=np.ones((2)), iterations=1)
 
         return mask, vector
-
 
     def make_chunks(self, spec):
         nr_chunks = spec.shape[1] // self.chunk_duration
@@ -124,7 +148,7 @@ class DataPreparator(object):
         chunks = []
         for chunk in spec_split:
             (chunk_frequ, chunk_length) = chunk.shape
-            if chunk_length  < self.chunk_duration:
+            if chunk_length < self.chunk_duration:
                 padded_chunk = np.zeros((chunk_frequ, self.chunk_duration))
                 padded_chunk[:, :chunk_length] = chunk
                 chunk = padded_chunk
