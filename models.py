ef441742eeb1deda5d065b25dd36fe2befb5fc13
Felix Berkhahn <felix.berkhahn@gmail.com>
2017-01-02 18:04:32 +0100

    bn in usual order

--- models.py
+++ models.py
@@ -13,36 +13,36 @@ def model_paper(nb_species, input_size): # Model from paper, adaption due to dif
     model.add(AveragePooling2D(pool_size=(2,2), input_shape=(nb_f_steps, nb_t_steps, 1)))
     # model.add(Dropout(0.2,input_shape=(1, nb_f_steps, nb_t_steps)))
     model.add(Dropout(0.2))
-    model.add(BatchNormalization())
     model.add(Convolution2D(nb_filters,kernel_size,kernel_size,subsample=(1,2),border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
         
     model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(BatchNormalization())
     model.add(Convolution2D(nb_filters, kernel_size, kernel_size,subsample=(1,1), border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(BatchNormalization())
     model.add(Convolution2D(2*nb_filters, kernel_size, kernel_size, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(BatchNormalization())
     model.add(Convolution2D(4*nb_filters, kernel_size, kernel_size, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(BatchNormalization())
     model.add(Convolution2D(4*nb_filters, 3, 3, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Dropout(0.4))
     model.add(Flatten())
 
-    model.add(BatchNormalization())
     model.add(Dense(1024))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
 
     model.add(Dropout(0.4))
