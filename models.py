d7c249c62568169a6578b9de6a6237ba120ff6ca
Felix Berkhahn <felix.berkhahn@gmail.com>
2017-01-03 18:58:08 +0100

    bugfix in model (use less maxpooling as intented) + take away dropout (shouldnt be necessary in conjunction with bn)

--- models.py
+++ models.py
@@ -10,20 +10,20 @@ def model_paper(nb_species, input_size): # Model from paper, adaption due to dif
     (nb_f_steps, nb_t_steps) = input_size
     model = Sequential()
 
-    model.add(AveragePooling2D(pool_size=(2,2), input_shape=(nb_f_steps, nb_t_steps, 1)))
-    # model.add(Dropout(0.2,input_shape=(1, nb_f_steps, nb_t_steps)))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(nb_filters,kernel_size,kernel_size,subsample=(1,2),border_mode='same'))
+    # model.add(AveragePooling2D(pool_size=(2,2), input_shape=(nb_f_steps, nb_t_steps, 1)))
+    # model.add(Dropout(0.2, input_shape=(nb_f_steps, nb_t_steps, 1)))
+    # model.add(Dropout(0.2))
+    model.add(Convolution2D(nb_filters,kernel_size,kernel_size,subsample=(1,2),border_mode='same',input_shape=(nb_f_steps, nb_t_steps, 1)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
         
     model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Convolution2D(nb_filters, kernel_size, kernel_size,subsample=(1,1), border_mode='same'))
+    model.add(Convolution2D(2*nb_filters, kernel_size, kernel_size,subsample=(1,1), border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(Convolution2D(2*nb_filters, kernel_size, kernel_size, border_mode='same'))
+    model.add(Convolution2D(4*nb_filters, kernel_size, kernel_size, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -36,16 +36,16 @@ def model_paper(nb_species, input_size): # Model from paper, adaption due to dif
     model.add(Convolution2D(4*nb_filters, 3, 3, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    # model.add(MaxPooling2D(pool_size=(2,2)))
 
-    model.add(Dropout(0.4))
+    # model.add(Dropout(0.4))
     model.add(Flatten())
 
     model.add(Dense(1024))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
 
-    model.add(Dropout(0.4))
+    # model.add(Dropout(0.4))
     model.add(Dense(nb_species))
     model.add(Activation('softmax'))
 
